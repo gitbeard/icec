ed64a35eeb52c22e313fbf6d32fda0759666dded
unknown <WindowsXP@.(none)>
2012-07-03 20:53:53 -0700

    Improved dynamic form.  Learned about mass_assignment.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  attr_accessible :description, :name
+  attr_accessible :description, :name, :tasks_attributes
   has_many :tasks
   accepts_nested_attributes_for :tasks, :reject_if => :all_blank, :allow_destroy => true
 end
